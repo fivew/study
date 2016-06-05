930b12b144cd21005929b136a8d479bd09e3ce9a
hyf <=>
2016-06-05 11:22:24 +0800

    update: go

---
+++ go/init_1/server2.go
@@ -0,0 +1,34 @@
+// server2 for count smilple web
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Printf("%v\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count = %d\n", count)
+	mu.Unlock()
+}
